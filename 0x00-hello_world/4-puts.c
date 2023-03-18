21d5cd96d7af924bb47d97c1c5827b730a380883
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-03-18 02:54:30 +0200

    First c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
-#!bin/bash
+#!/bin/bash
+
 #include <stdio.h>
-
-int main(void){
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 	puts("Programming is like building a multilingual puzzle");
-	return (0);
-}
-
+	return (0); }
