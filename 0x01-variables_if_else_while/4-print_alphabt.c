45e3c9b6e3bdad2e76504dfe8061db0503ffd08f
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-03-20 05:28:01 +0200

    lower except e & q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 96;
+
+	while (a < 122)
+	{
+		a++;
+		if(a==113 || a==101)
+			continue;
+		putchar(a);
+	}
+	putchar('\n');
+
+	return (0); }
