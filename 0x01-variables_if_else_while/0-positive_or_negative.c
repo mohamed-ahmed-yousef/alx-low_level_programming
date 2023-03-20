2f2c80556ae652103ea654bf711485e809562168
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-03-20 04:10:46 +0200

    pis positive or negative or zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,11 +6,16 @@
  */
 int main(void)
 {
-	int n=19;
-	if(n>0)
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+
+	if (n > 0)
 		printf("is positive\n");
-	else if (n<0)
+	else if (n < 0)
 		printf("is negative\n");
-	else 
+	else
 		printf("is zero\n");
 	return (0); }
