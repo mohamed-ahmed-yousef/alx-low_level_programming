f3ab1ff65ef369590889063fa507f687cc3dc632
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-03-22 22:39:32 +0200

    1-10 in c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	for (int i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0); }
+
