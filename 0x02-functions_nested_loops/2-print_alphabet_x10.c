a5c9c33151a590a6a9a16053b35433eeac145ac2
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-03-23 19:54:58 +0200

    a-z ten times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,7 +4,7 @@
 *
 * Return: Always 0
 */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	int i = 0;
 	int j = 0;
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-    print_alphabet();
-    return (0);
+    print_alphabet_x10();
+	return (0);
 }
