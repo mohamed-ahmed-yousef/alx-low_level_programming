390058483f9ad503cf266737cb3f5ca912d4fd53
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-03-23 20:16:21 +0200

    islower function

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+/**
+* _islower - find if the char is lower or upper.
+*
+*@c: is an integer value passed from main function.
+* Return: Always 0
+*
+*/
+int _islower(int c)
+{
+	if (c >= 97 && c < 123)
+	return (1);
+	return (0); }
