34b06fec61b201394c887eec49b969aa57e006bd
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-03-23 20:59:09 +0200

    implement abs value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _abs - find if the char is lower or upper.
+*
+*@c: is an integer value passed from main function.
+* Return: Always 0
+*
+*/
+int _abs(int c)
+{
+	if (c > 0)
+	return (c);
+	else if (c < 0)
+	return (-c);
+	else
+	return c; }
