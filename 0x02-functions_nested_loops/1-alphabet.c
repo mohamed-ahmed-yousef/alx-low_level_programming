9ae77434bd3fe09d434a55eb96d8c5c5faa8fae5
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-03-23 05:26:17 +0200

    a-z with _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_alphabet - print string
+*
+* Return: Always 0
+*/
+
+void print_alphabet(void)
+{
+	for (int i=97; i < 123; i++) 
+	_putchar(i);
+	_putchar('\n');
+}
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
