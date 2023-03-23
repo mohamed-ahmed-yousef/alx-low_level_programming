09d9d4bf8cb8481994b53fa2ef05cbb4b0f02c66
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-03-23 20:27:18 +0200

    islower & isupper function

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,11 @@
 /**
-* _islower - find if the char is lower or upper.
+* _isalpha - find if the char is lower or upper.
 *
 *@c: is an integer value passed from main function.
 * Return: Always 0
 *
 */
-int _islower(int c)
+int _isalpha(int c)
 {
 	if ((c >= 97 && c < 123) || (c>=65 && c<91))
 	return (1);
