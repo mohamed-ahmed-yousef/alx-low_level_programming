a0d074df0a275f6e94840fd4f6f7b80451efb017
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-03-24 02:49:30 +0200

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* add - print last digit of the number.
+*@a: is the first Number
+*@b: is the second Number
+* Return: return the sum of two number
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
