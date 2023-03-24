c0f7a02b6c52ddf5fee38b13d53206c659ffd84b
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-03-24 02:07:44 +0200

    hours & mintues

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+* jack_bauer - print last digit of the number.
+*
+*@c: is an integer value passed from main function.
+* Return: Always 0
+*
+*/
+void jack_bauer(void)
+{
+	int i = 0, j = 0, k = 0, l = 0;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (k = 0; k < 6; k++)
+			{
+				for (l = 0; l < 10; l++)
+				{
+					_putchar('0' + i);
+					_putchar('0' + j);
+					_putchar(':');
+					_putchar('0' + k);
+					_putchar('0' + l);
+					_putchar('\n');
+					if (i == 2 && j == 3 && k == 5 && l == 9)
+					return;
+
+				}
+			}
+		}
+	}
+}
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,16 +1,12 @@
-#include <limits.h>
 #include "main.h"
-#include <stdio.h>
+
 /**
- * main - check the code for Holberton School students.
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int r;
-
-	r = print_last_digit(INT_MIN);
-	_putchar('\n');
-	return (0);
+    jack_bauer();
+    return (0);
 }
