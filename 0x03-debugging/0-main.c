428f19297c5d22a8439d61fd5150df50b5f491ca
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-03-25 01:07:49 +0200

    changed

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* main - test function for  positive or negative
+* main - Test function for positive or negative
 * Return: 0
 */
 
@@ -12,5 +12,5 @@ int main(void)
         i = 0;
         positive_or_negative(i);
 
-        return (0);
+return (0);
 }
