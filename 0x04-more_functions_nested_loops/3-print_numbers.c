dc349470d8024555b50ae1ad4039bf02beac16ba
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-03-25 04:18:58 +0200

    digits

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_numbers -  function check upper case for string
+*
+* print all digit from 0 -9
+* Return: Always return 0 or 1
+*/
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (i = 48; i < 58; i++)
+	_putchar(i);
+	_putchar('\n');
+}
