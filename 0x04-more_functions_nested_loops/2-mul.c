7797cd363a6312515fa8adbbc9dd86e1aa04bef4
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-03-25 04:13:10 +0200

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* mul -  function check upper case for string
+*
+*@a: the char
+*@b: the char
+* Return: Always return 0 or 1
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
