40e0d3d83891f388d92c939b9e1c3ee9e7ba6eb6
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-04-13 17:46:28 +0200

    reverse string

--- 0x05-pointers_arrays_strings/exe.c
+++ 0x05-pointers_arrays_strings/exe.c
@@ -1,15 +1,19 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - check the code for
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char *str;
+    int array[5];
 
-    str = "01233456789";
-    puts_half(str);
+    array[0] = 98;
+    array[1] = 402;
+    array[2] = -198;
+    array[3] = 298;
+    array[4] = -1024;
+    print_array(array, 5);
     return (0);
 }
