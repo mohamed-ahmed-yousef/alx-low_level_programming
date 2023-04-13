79fb9a84ac6639726617e8dcd8bc958dcd8ec67b
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-04-13 17:40:02 +0200

    reverse string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,8 +7,14 @@
 void puts_half(char *str)
 {
 	int n = strlen(str);
-	int x = n / 2;
 	int i = 0;
+	int x;
+
+	if (n % 2 == 0)
+		x = n / 2;
+	else
+		x = n / 2 + 1;
+
 
 	for (i = x; i < n; i++)
 	{
--- 0x05-pointers_arrays_strings/exe.c
+++ 0x05-pointers_arrays_strings/exe.c
@@ -1,19 +1,15 @@
 #include "main.h"
 
 /**
- * main - check the code for
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    int array[5];
+    char *str;
 
-    array[0] = 98;
-    array[1] = 402;
-    array[2] = -198;
-    array[3] = 298;
-    array[4] = -1024;
-    print_array(array, 5);
+    str = "01233456789";
+    puts_half(str);
     return (0);
 }
