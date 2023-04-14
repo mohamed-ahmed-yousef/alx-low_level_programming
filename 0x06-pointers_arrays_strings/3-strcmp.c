3e0317073a5b31bccd846ee23983566393f2ba82
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-04-14 14:10:58 +0200

    More pointer_array_String in c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,23 +1,35 @@
 #include "main.h"
-
 /**
- * _strcmp -  function that compares two strings.
- * @s1: primera cadena.
- * @s2: segunda cadena.
- * Return: 0.
- */
-
+ * 
+*/
 int _strcmp(char *s1, char *s2)
 {
-	int res = 0;
+	int len1 = 0;
+	int len2 = 0;
+	int loop = 0;
+	int i = 0;
 
-	while (*s1 == *s2 && *s1 != '\0')
+	while (s1[len1] != '0')
+	len1++;
+	while (s2[len2] != '0')
+	len2++;
+
+	if (len1 > len2)
+	loop = len2;
+	else
+	loop = len1;
+
+
+	for (i = 0; i < len1; i++)
 	{
-		s1++;
-		s2++;
+		if (s1[i] != s2[i])
+		{
+			return ((int)s1[i] - (int)s2[i]);
+		}
 	}
-	if (s1 != s2)
-		res = *s1 - *s2;
-
-	return (res);
+	if (len1 > len2)
+    return (int)s1[i];
+    else if (len1 < len2)
+    return -(int)s2[i];
+	return (0);
 }
--- 0x06-pointers_arrays_strings/try.c
+++ 0x06-pointers_arrays_strings/try.c
@@ -1,22 +1,41 @@
 #include <stdio.h>
-
+#include <string.h>
 int main(void)
 {
+    char s2[] = "Hello";
+    char s1[] = "H";
+    printf("%d\n",strcmp(s1,s1));
+    
+    int len1 = 0;
+    int len2 = 0;
+    int loop=0;
+    int i=0;
 
-	int len=0;
-	int me=0;
-    char dest[98] = "Hello ";
-    char src[] = "World!\n";
+    while (s1[len1] != '\0')
+    len1++;
+    while (s2[len2] != '\0')
+    len2++;
+    
+    if (len1>len2)
+    loop = len2;
+    else
+    loop = len1;
 
-	while (dest[me] != '\0')
-	me++;
 
-    printf("%d",me);
-	while (src[len] != '\0')
-	{
-		dest[me+len-1]=src[len];
-		len++;
-	}
-    printf("%s",dest);
-    return (0);
+    for (i = 0; i < len1; i++)
+    {
+        if (s1[i] != s2[i])
+        {
+        printf("%d",(int)s1[i]-(int)s2[i]);
+        return 0;
+        }
+    }
+    if (len1 > len2)
+    printf("%d",(int)s1[i]);
+    else if (len1 < len2)
+     printf("%d",(int)s1[i]);
+    printf(("%d"), 0);
+
+
+
 }
