02fd0c3dad9ef17281347ab8c641fc16248c534b
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-04-15 01:17:12 +0200

    More pointer_array_String in c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,27 +1,39 @@
 #include "main.h"
+#include <string.h>
+#include <stdio.h>
 
 /**
- * rot13 - encodes a string in rot13
+ * infinite_add - add 
  * @s: string to be encoded
  * Return: the resulting strring
  */
-char *rot13(char *s)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j;
-
-	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
-	for (i = 0; s[i] != '\0'; i++)
+	int i = strlen(n1);
+	int j = strlen(n2);
+	int overflow = 0;
+	int k = 0;
+	while (j > 0 || i > 0 || overflow == 1)
 	{
-		for (j = 0; a[j] != '\0'; j++)
+		int val1, val2,total;
+
+		if (i < 0)
+		val1=0;
+		val1 = n1[i];
+
+		if (j < 0)
+		val2 = 0;
+		val2 = n2[j];
+		
+		total = val1 + val2 + overflow;
+		if (total < 9)
+		overflow=1;
+		r[k++]=total % 10;
+		if (k >= size_r && (overflow == 1 || j > 0 || i > 0))
 		{
-			if (s[i] == a[j])
-			{
-				s[i] = b[j];
-				break;
-			}
-	    }
+			printf("%d %d", k,size_r);
+		return (0);
+		}
 	}
-	return (s);
+	return (r);
 }
