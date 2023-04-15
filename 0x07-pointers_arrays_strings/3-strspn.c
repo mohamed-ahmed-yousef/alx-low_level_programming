1153dafa82f56ccbff3dbe4abc45b019815513ab
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-04-15 16:29:17 +0200

    pointers_arrays_strings in c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,4 +1,31 @@
 #include "main.h"
 /**
- * 
+ * _strspn -  gets the length of a prefix substring
+ * @s: main string.
+ * @accept: secondary string.
+ * .
+ * Return: return the number.
 */
+unsigned int _strspn(char *s, char *accept)
+{
+int i = 0;
+int j = 0;
+unsigned int count = 0;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+int check = 0;
+for (j = 0; s[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+count++;
+check = 1;
+break;
+}
+}
+if (!check)
+return (count);
+}
+return (count);
+}
--- 0x07-pointers_arrays_strings/try.c
+++ 0x07-pointers_arrays_strings/try.c
@@ -1,23 +1,10 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
-#include <stddef.h>
 
-int main()
-{
-    char *str="Hello";
-    int i =0;
-    char c = 'o';
-    int curr=0;
-    
-
-    for (i = 0; i < strlen(str); i++)
-    {
-        curr=*str++;
-        if (curr == c)
-        {
-            printf("%d", *(str -1) + '0');
-        }
-    }
-    
+int main() {
+    const char* str1 = "hello world";
+    const char* str2 = "ehlw";
+    size_t len = strspn(str1, str2);
+    printf(" %zu\n", len);
+    return 0;
 }
