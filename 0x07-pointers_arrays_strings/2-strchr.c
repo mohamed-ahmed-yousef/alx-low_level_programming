4cb922ece4916f94c01b9933b30b01761a8ea231
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-04-15 14:56:50 +0200

    pointers_arrays_strings in c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr - locate a character in the string.
+ * @s: string to find char in it.
+ * @c: character.
+ * .
+ * Return: return first accurance of char in string.
+*/
+char *_strchr(char *s, char c)
+{
+int i = 0;
+int cur;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+cur = *s++;
+if (cur == c)
+return (s - 1);
+}
+return (0);
+}
