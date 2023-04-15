d50bdf4db5b2f9cd3536c09c72e54e77f7d165c5
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-04-16 01:08:32 +0200

    pointers_arrays_strings in c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,11 +1,24 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * 
+ * print_diagsums - dealing with 2-d array.
+ * @a: my array
+ * @size: size of array.
 */
 void print_diagsums(int *a, int size)
 {
-    int len = sizeof(a);
+int i, j, count1 = 0, count2  = 0;
 
-    printf("%d\n", len);
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+if (i == j)
+count1 += a[i * size + j];
+
+if (i + j == size - 1)
+count2 += a[i * size + j];
+}
+}
+printf("%d, %d\n", count1, count2);
 }
