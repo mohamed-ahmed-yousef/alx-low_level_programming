0c50a6da641de8f13c7c051a6d60d2317f951c3f
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-04-18 16:08:05 +0200

    Recursion In C

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef _HEADER_
 #define _HEADER_
 
+/**
+ * struct dog - Dog attributes
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Description: The attributes of a dog.
+ */
 struct dog
 {
 	char *name;
@@ -8,7 +16,9 @@ struct dog
 	char *owner;
 };
 
-
+/**
+ * dog_t - Typedef for dog structure
+ */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
