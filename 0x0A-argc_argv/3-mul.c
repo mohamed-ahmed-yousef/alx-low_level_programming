c1347ff7e0e47b33c272bbade6e37f6ff5b22eab
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-04-18 21:17:34 +0200

    argc_argv In C

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print program name
+ * @argc: argument count
+ * @argv: argument vector.
+ * Return: return 0 if sucessful.
+*/
+int main(int argc, char *argv[])
+{
+    (void)argc;
+    if (argc == 1)
+        printf("Error\n");
+    else
+    {
+        int num1 = atoi(argv[1]);
+        int num2 = atoi(argv[2]);
+        printf("%d\n", num1 * num2);
+    }
+    return (0);
+}
