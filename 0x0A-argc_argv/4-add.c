b6fa90aa2269ecdd8355d8a3107991812fee13a1
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-04-18 22:01:46 +0200

    argc_argv In C

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,20 +9,20 @@
 */
 int main(int argc, char *argv[])
 {
-    int result = 0, i;
+int result = 0, i;
 
-    for (i = 1; i < argc; i++)
-    {
-        char c = argv[i][0];
-        int n = atoi(argv[i]);
+for (i = 1; i < argc; i++)
+{
+char c = argv[i][0];
+int n = atoi(argv[i]);
 
-        if (!isdigit(c))
-        {
-            printf("Error\n");
-            return (1);
-        }
-        result += n;
-    }
-    printf("%d\n", result);
-    return (0);
+if (!isdigit(c))
+{
+printf("Error\n");
+return (1);
+}
+result += n;
+}
+printf("%d\n", result);
+return (0);
 }
