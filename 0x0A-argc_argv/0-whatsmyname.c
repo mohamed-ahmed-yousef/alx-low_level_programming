bd4db4449ce78403fdd829f67ccd004d5a67198c
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-04-18 20:35:13 +0200

    argc_argv In C

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print program name
+ * @argc: argument count
+ * @argv: argument vector.
+ * Return: return 0 if sucessful.
+*/
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,6 +7,6 @@
 */
 int main(int argc, char *argv[])
 {
-printf("%s\n", argv[0]);
+printf("%d\n", argc - 1);
 return (0);
 }
