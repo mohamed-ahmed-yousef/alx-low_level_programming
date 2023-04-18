bc2263942a4ff00dce3d93762caed3e4f635ee99
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-04-18 22:01:26 +0200

    argc_argv In C

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - print program name
+ * @argc: argument count
+ * @argv: argument vector.
+ * Return: return 0 if sucessful.
+*/
+int main(int argc, char *argv[])
+{
+    int result = 0, i;
+
+    for (i = 1; i < argc; i++)
+    {
+        char c = argv[i][0];
+        int n = atoi(argv[i]);
+
+        if (!isdigit(c))
+        {
+            printf("Error\n");
+            return (1);
+        }
+        result += n;
+    }
+    printf("%d\n", result);
+    return (0);
+}
