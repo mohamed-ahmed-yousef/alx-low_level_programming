1c641feeeb866e2d800a706b268dd43dc0b02260
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-04-18 21:22:57 +0200

    argc_argv In C

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,15 +9,13 @@
 int main(int argc, char *argv[])
 {
 (void)argc;
-if (argc == 1)
+if (argc < 3)
 printf("Error\n");
 else
 {
 int num1 = atoi(argv[1]);
 int num2 = atoi(argv[2]);
-long int a = num1;
-long int b = num2;
-printf("%ld\n", a * b);
+printf("%d\n", num1 * num2);
 }
 return (0);
 }
