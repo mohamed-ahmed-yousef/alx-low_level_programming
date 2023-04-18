94bb4362dc1a14916297d1b63ec84329f5a4936b
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-04-18 22:39:50 +0200

    argc_argv In C

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print program name
+ * @argc: argument count
+ * @argv: argument vector.
+ * Return: return 0 if sucessful.
+*/
+int main(int argc, char *argv[])
+{
+    int i = 0, count = 0,num ;
+    int arr[5] = {25, 10, 5, 2, 1};
+
+    if (argc == 1)
+    {
+        printf("Error\n");
+        return (1);
+    }
+    num = atoi(argv[1]);
+
+    while (num > 0)
+    {
+        if (num >= arr[i])
+        {
+            num -= arr[i];
+            count++;
+        }
+        else
+            i++;
+    }
+    printf("%d\n", count);
+    return (0);
+}
