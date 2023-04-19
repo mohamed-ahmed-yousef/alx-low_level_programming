1e1b20742e983b3c02b094574805ed506d91a94b
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-04-19 14:47:40 +0200

    malloc In C

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,16 +9,18 @@
 */
 char *_strdup(char *str)
 {
-int len = 1, i = 0;
+int len = 0, i = 0;
 char *new_str;
+if (str == NULL)
+return (NULL);
 
-while (str && str[len] != '\0')
+while (str[len] != '\0')
 len++;
-new_str = malloc(len *sizeof(*str));
+new_str = malloc((len + 1) * sizeof(*str));
 if (new_str == NULL)
 return (NULL);
 
-while (str && str[i] != '\0')
+while (str[i] != '\0')
 {
 new_str[i] = str[i];
 i++;
