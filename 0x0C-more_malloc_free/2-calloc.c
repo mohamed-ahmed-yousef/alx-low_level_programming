8ac379899b96fc6453861958692b1a6ec5f4a448
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-04-20 15:55:35 +0200

    more_malloc_free in c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,12 +10,19 @@
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-char *arr = malloc(size * nmemb);
+char *arr;
 unsigned int i = 0;
 
+if (size == 0)
+return (NULL);
+
+arr = malloc(size * nmemb);
+
+if (arr == NULL)
+return (NULL);
+
 for (i = 0; i < nmemb; i++)
-{
 arr[i] = 0;
-}
+
 return (arr);
 }
