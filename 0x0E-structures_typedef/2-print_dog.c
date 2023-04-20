08d476a1f3b50338946325495896b03fba6724cd
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-04-21 00:12:20 +0200

    structures_typedef  in c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,10 +13,10 @@ if (d->name)
 printf("Name: %s\n", d->name);
 else
 printf("Name: (nil)\n");
-if (d->age)
+
 printf("Age: %.6f\n", d->age);
-else
-printf("Age: (nil)\n");
+
+
 if (d->owner)
 printf("Owner: %s\n", d->owner);
 else
--- 0x0E-structures_typedef/exe.c
+++ 0x0E-structures_typedef/exe.c
@@ -1,18 +1,19 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "dog.h"
 
 /**
- * main - check the code
+ * main - check the code .
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    dog_t my_dog;
+	struct dog my_dog;
 
-    my_dog.name = "Poppy";
-    my_dog.age = 3.5;
-    my_dog.owner = "Bob";
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-    return (0);
+	my_dog.name = NULL;
+	my_dog.age = .0f;
+	my_dog.owner = NULL;
+	print_dog(&my_dog);
+	return (0);
 }
