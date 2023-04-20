1244de67e8cb5e41f51350e04c0c1f8f2b5991b1
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-04-20 23:59:45 +0200

    structures_typedef  in c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,6 +12,8 @@ char *name;
 char *owner;
 float age;
 };
-void init_dog(struct dog *d, char *name, float age, char *owner);
+typedef struct dog dog_t;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
