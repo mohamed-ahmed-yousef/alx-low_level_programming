06cc5b23d5380a253e728fd49131ea99820d31d4
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-04-20 17:58:17 +0200

    more_malloc_free in c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main -  program that multiplies two positive numbers.
+ * -----------
+ * @agrc: argument count.
+ * @argv: argument vector.
+ * -----------
+ * Return: result, followed by a new line
+ * -     : If the number of arguments is incorrect, 
+ *          print Error & exit with a status of 98
+ * -     : num1 and num2 should only be composed of 
+ *          digits. If not, print Error
+*/
+int main(int agrc, char *argv[])
+{
+
+
+}
