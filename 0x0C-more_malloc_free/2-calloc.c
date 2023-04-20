6709d57adcf5d3aa80ac549c2b4ffa2026fb81c9
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-04-20 16:01:59 +0200

    more_malloc_free in c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 /**
  * _calloc - allocates memory for an array, using malloc
- * @nmemb: 1st string.
- * @size: 2nd string
+ * @nmemb: number of element.
+ * @size: size of each element
  * Return: If nmemb or size is 0, then _calloc returns NULL
  *         If malloc fails, then _calloc returns NULL
 */
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 char *arr;
 unsigned int i = 0;
 
-if (size == 0)
+if (size == 0 || nmemb == 0)
 return (NULL);
 
 arr = malloc(size * nmemb);
@@ -21,7 +21,7 @@ arr = malloc(size * nmemb);
 if (arr == NULL)
 return (NULL);
 
-for (i = 0; i < nmemb; i++)
+for (i = 0; i < nmemb * size; i++)
 arr[i] = 0;
 
 return (arr);
--- 0x0C-more_malloc_free/exe.c
+++ 0x0C-more_malloc_free/exe.c
@@ -1,7 +1,8 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+void *_calloc(unsigned int, unsigned int);
 
 /**
  * simple_print_buffer - prints buffer in hexa
@@ -10,41 +11,45 @@
  *
  * Return: Nothing.
  */
-void simple_print_buffer(char *buffer, unsigned int size)
+void simple_print_buffer(int *buffer, unsigned int size)
 {
-    unsigned int i;
+	unsigned int i;
 
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
+	}
+	printf("\n");
 }
 
 /**
- * main - check the code
+ * main - check the code .
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char *a;
+	int *a;
+	unsigned int nmemb;
 
-    a = _calloc(98, sizeof(char));
-    strcpy(a, "Best");
-    strcpy(a + 4, " School! :)\n");
-    a[97] = '!';
-    simple_print_buffer(a, 98);
-    free(a);
-    return (0);
+	nmemb = 50;
+	a = _calloc(nmemb, sizeof(*a));
+	if (a == NULL)
+	{
+		printf("Failed\n");
+		return (1);
+	}
+	simple_print_buffer(a, nmemb);
+	free(a);
+	return (0);
 }
