bcb29f37e2a63c6851fe4b37a85f65cce3e7e6b7
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-04-20 12:25:26 +0200

    malloc In C

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,119 @@
+#include "main.h"
+#include "stdio.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+int count_word(char *str);
+int *volume_Word(char *str, int w, int *arr);
+char **store(char **matrix, int *volume, char *str);
+/**
+ * strtow -returns a pointer to an array of strings (words)
+ * @str: string to return from it array of string.
+ * Return:  NULL if str == NULL or str == ""
+*/
+char **strtow(char *str)
+{
+int w = count_word(str);
+char **matrix;
+int *volume;
+int arr[w];
+int size, i;
+
+volume = volume_Word(str,  w, arr);
+size = sizeof(volume) / sizeof(volume[0]);
+matrix = (char **) malloc(size *sizeof(int *));
+for (i = 0; i < size; i++)
+matrix[i] = (char *) malloc((volume[i] + 1) * sizeof(int));
+
+matrix = store(matrix, volume, str);
+
+return (matrix);
+}
+/**
+ * count_word - count the word in string.
+ * @str: string to count word from it.
+ * Return: return no. of words.
+*/
+
+int count_word(char *str)
+{
+int w = 0, i = 0;
+char c = ' ';
+
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] == ' ' && c != ' ')
+w++;
+c = str[i];
+}
+if (c != ' ')
+w++;
+return (w);
+}
+/**
+ * volume_Word - To give me the volume of each word.
+ * @str: the string.
+ * @w: the number of word.
+ * @arr: my array.
+ * Return: always return array.
+*/
+int *volume_Word(char *str, int w,  int *arr)
+{
+int i = 0, start = 0, c = 0, j = 0;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+{
+start = i;
+c = 1;
+}
+else if (c == 1 && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+{
+arr[j] = i + 1 - start;
+j++;
+start = 0;
+c = 0;
+}
+}
+
+return (arr);
+}
+/**
+ * store - function that store words in pointer of string
+ * @matrix: pointer of string.
+ * @volume: array of volume;
+ * @str: my string.
+ * Return: return the martix;
+*/
+char **store(char **matrix, int *volume, char *str)
+{
+char *temp;
+int i = 0, start = 0, end = 0, c = 0, j = 0, t = 0, m = 0;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+{
+temp = (char *) malloc((volume[j] + 1) * sizeof(char));
+if (temp == NULL)
+return (NULL);
+j++;
+c = 1;
+}
+if (c)
+{
+temp[t] = str[i];
+t++;
+}
+if (c == 1 && (str[i] == ' ' || str[i + 1] == '\0'))
+{
+temp[t] = '\0';
+matrix[m] = temp;
+m++;
+t = 0;
+c = 0;
+}
+}
+return (matrix);
+}
--- 0x0B-malloc_free/exe.c
+++ 0x0B-malloc_free/exe.c
@@ -1,24 +1,38 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-char *argstostr(int, char **);
+/**
+ * print_tab - Prints an array of string
+ * @tab: The array to print
+ *
+ * Return: nothing
+ */
+void print_tab(char **tab)
+{
+    int i;
+
+    for (i = 0; tab[i] != NULL; ++i)
+    {
+        printf("%s\n", tab[i]);
+    }
+}
 
 /**
- * main - check the code
+ * main - check the code for ALX School students.
  *
- * Return: Always 0.
+ * Return: 1 if an error occurred, 0 otherwise
  */
 int main(void)
 {
-	char *s;
+    char **tab;
 
-	s = argstostr(0, NULL);
-	if (s == NULL)
-	{
-		printf("Failed\n");
-		return (1);
-	}
-	printf("%s", s);
-	free(s);
-	return (0);
+    tab = strtow("      ALX School         #cisfun df");
+    if (tab == NULL)
+    {
+        printf("Failed\n");
+        return (1);
+    }
+    print_tab(tab);
+    return (0);
 }
