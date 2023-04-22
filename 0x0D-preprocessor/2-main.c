0f988f54f39a460f9974b47359740cd14a10e442
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-04-22 22:05:16 +0200

    preprocessor in c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - preints the name of the file
- * Return: Always 0 (Success)
- */
+ * main - prints the name of the file it was compiled from
+ * + new line
+ * Return: always return (0) if sucessfully.
+*/
 int main(void)
 {
-	printf("%s\n", __FILE__);
-	return (0);
+printf(__FILE__ "\n");
+return (0);
 }
