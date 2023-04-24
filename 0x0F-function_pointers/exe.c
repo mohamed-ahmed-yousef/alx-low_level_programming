a5485b2e33bb2ee61bba71946cdb33fd9d7b0d43
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-04-24 17:31:37 +0200

    function_pointers in c

--- 0x0F-function_pointers/exe.c
+++ 0x0F-function_pointers/exe.c
@@ -45,11 +45,11 @@ int main(void)
     int array[20] = {0, -98, 98, 402, 1024, 4096, -1024, -98, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
     int index;
 
-    index = int_index(array, 20, is_98);
+    index = int_index(NULL, 20, is_98);
     printf("%d\n", index);
-    index = int_index(array, 20, abs_is_98);
+    index = int_index(array, 0, abs_is_98);
     printf("%d\n", index);
-    index = int_index(array, 20, is_strictly_positive);
+    index = int_index(array, 1, is_strictly_positive);
     printf("%d\n", index);
     return (0);
 }
