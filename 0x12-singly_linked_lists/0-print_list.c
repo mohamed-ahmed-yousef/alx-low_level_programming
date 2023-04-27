ff6ff905e7a672830d42958d1aa3b024deba56c6
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-04-28 02:46:57 +0300

    Singly linked lists in C

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,8 +2,8 @@
 #include "lists.h"
 /**
  * print_list - print all element in the linked list.
- * @list_t: linked list passed.
- * 
+ * @h: linked list passed.
+ * Return: return the no.of element in struct.
 */
 
 size_t print_list(const list_t *h)
@@ -13,7 +13,7 @@ size_t n = 0;
 while (h != NULL)
 {
 if (h->str)
-printf("%u %s\n", h->len, h->str);
+printf("[%u] %s\n", h->len, h->str);
 else
 printf("[0] (nil)\n");
 h = h->next;
