e1a5aa6fb90b960028606cfd64850ce3b8065f75
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-04-28 03:51:26 +0300

    Singly linked lists in C

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_node_end - Add node to the end.
+ * @head: the head of the node.
+ * @str: string.
+ * Return: return the linkedlist.
+*/
+list_t *add_node_end(list_t **head, const char *str)
+{
+list_t *store = malloc(sizeof(list_t));
+list_t *linked_list;
+int len = 0;
+
+while (str[len])
+len++;
+
+store->len = len;
+store->str = strdup(str);
+store->next = NULL;
+
+if (*head == NULL)
+*head = store;
+else
+{
+linked_list = *head;
+
+while (linked_list->next != NULL)
+linked_list = linked_list->next;
+linked_list->next = store;
+}
+}
