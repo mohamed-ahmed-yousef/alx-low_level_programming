967431afe2cdc4a751c3329390d57fa04b3dac08
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-08-23 19:06:11 +0300

    upload File

---
+++ 0x18-dynamic_libraries/mathOperation.c
@@ -0,0 +1,54 @@
+/**
+ * add - function to call
+ *@a: first Number to add.
+ *@b: second Number to add.
+ * return - sum
+*/
+int add(int a, int b)
+{
+	return a + b;
+}
+
+/**
+ * sub - function to call
+ *@a: first Number .
+ *@b: second Number.
+ * return - sub
+*/
+int sub(int a, int b)
+{
+	return a - b;
+}
+
+/**
+ * mul - function to call
+ *@a: first Number .
+ *@b: second Number.
+ * return - mul
+*/
+int mul(int a, int b)
+{
+	return a * b;
+}
+
+/**
+ * div - function to call
+ *@a: first Number .
+ *@b: second Number.
+ * return - div
+*/
+int div(int a, int b)
+{
+	return a / b;
+}
+
+/**
+ * mod - function to call
+ *@a: first Number .
+ *@b: second Number.
+ * return - mod
+*/
+int mod(int a, int b)
+{
+	return a % b;
+}
