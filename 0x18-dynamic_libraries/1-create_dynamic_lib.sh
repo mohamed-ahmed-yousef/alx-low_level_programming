029db38d63590fe8147fb99887f31ed12057d677
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-08-23 18:49:03 +0300

    upload File

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-gcc -c -fPIC *.c # to Generate object file.
-gcc -shared -o liball.so *.o # but all .o in one and -o to specific the name.
-
+gcc -fPIC -c *.c
+gcc -shared -o liball.so *.o
