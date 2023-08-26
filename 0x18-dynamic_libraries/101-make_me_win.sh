aa7185fe23625dcf4a315eefb71aba69b04bd273
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-08-26 14:08:16 +0300

    upload File

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget  https://raw.githubusercontent.com/mohamedahmed-cloud/alx-low_level_programming/main/0x18-dynamic_libraries/libtest.so
-export LD_PRELOAD=libtest.so
+wget /tmp/ https://raw.githubusercontent.com/mohamedahmed-cloud/alx-low_level_programming/main/0x18-dynamic_libraries/libtest.so
+export LD_PRELOAD=/tmp/libtest.so
